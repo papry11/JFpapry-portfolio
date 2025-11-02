5cd6dc263a5bd6b58f3d3473bf02e5fe764bfa7e
papry <jannatulferdouspapry115@gmail.com>
2025-11-02 14:18:32 +0600

    new update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Header from '../components/Header'
 import ProjectCard from '../components/ProjectCard'
 import CounterSection from '../components/Countersection'
 import ExperienceAndSkills from '../components/ExperienceAndSkill'
+import Testimonal from '../components/Testimonal'
 
 
 const Home = () => {
@@ -12,7 +13,7 @@ const Home = () => {
       <CounterSection />
       <ProjectCard />
       <ExperienceAndSkills />
-     
+      <Testimonal/>
     </div>
   )
 }
